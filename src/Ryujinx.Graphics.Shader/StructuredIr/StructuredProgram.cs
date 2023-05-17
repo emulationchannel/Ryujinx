41b52e265fc558d6a228cf8cce2c904af8596295
gdk <gab.dark.100@gmail.com>
2023-05-16 21:22:11 -0300

    Remove now unused code

--- src/Ryujinx.Graphics.Shader/StructuredIr/StructuredProgram.cs
+++ src/Ryujinx.Graphics.Shader/StructuredIr/StructuredProgram.cs
@@ -262,10 +262,6 @@ namespace Ryujinx.Graphics.Shader.StructuredIr
                     {
                         context.Info.HelperFunctionsMask |= HelperFunctionsMask.AtomicMinMaxS32Shared;
                     }
-                    else if (operation.StorageKind == StorageKind.StorageBuffer)
-                    {
-                        context.Info.HelperFunctionsMask |= HelperFunctionsMask.AtomicMinMaxS32Storage;
-                    }
                     break;
                 case Instruction.MultiplyHighS32:
                     context.Info.HelperFunctionsMask |= HelperFunctionsMask.MultiplyHighS32;
@@ -288,10 +284,6 @@ namespace Ryujinx.Graphics.Shader.StructuredIr
                 case Instruction.StoreShared16:
                 case Instruction.StoreShared8:
                     context.Info.HelperFunctionsMask |= HelperFunctionsMask.StoreSharedSmallInt;
-                    break;
-                case Instruction.StoreStorage16:
-                case Instruction.StoreStorage8:
-                    context.Info.HelperFunctionsMask |= HelperFunctionsMask.StoreStorageSmallInt;
                     break;
                 case Instruction.SwizzleAdd:
                     context.Info.HelperFunctionsMask |= HelperFunctionsMask.SwizzleAdd;
